f01ffbd58b91a68eb7bbd0ee60c82c384966723f
elisamhh <elisamhurt@gmail.com>
2023-09-04 14:50:09 +0200

    Added first paragraph

--- challenge_day1/elisa.py
+++ challenge_day1/elisa.py
@@ -1,1 +1,8 @@
 print("Elisa")
+
+elisa_paragraph1 = '''
+Sari the cactus agreed with Martyna, acknowledging her short-lived blooms.
+Elisa the mushroom provided shelter and nourishment, Jesper the conifer offered year-round protection,
+and Dimma the succulent thrived in arid conditions. Thex stood united, content with their differences,
+and found purpose in their unique roles in the forest.
+'''
