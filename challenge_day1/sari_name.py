18f9b545bb1a514a9bbc61a296c8278ec27fc5c6
wmartynaa <wojmartynaa@gmail.com>
2023-09-04 14:59:30 +0200

    added a second paragraph

--- challenge_day1/sari_name.py
+++ challenge_day1/sari_name.py
@@ -6,3 +6,9 @@ a deciduous called Martyna, and a succulent called Dima.
 "Your flower is so short-lived, Sari" the deciduous said, "as a Cactus it only blooms once
 and then wilts off."
 '''
+
+sari_paragraph2 = '''
+At the beach everyone was extremely happppy. Elisa jumped over the waves. Jesper was diving. 
+Martyna was fishing. Dima and Sari were making sandcastles. The sunset was apppproaching and
+the sunlight reflected in the water.
+'''
