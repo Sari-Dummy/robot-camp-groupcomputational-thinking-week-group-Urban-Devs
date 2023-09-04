97b2c89884b35e756f628150803277bf7644a5c0
jssaakes <jespersaakes@gmail.com>
2023-09-04 15:48:28 +0200

    new paragraph

--- challenge_day1/jesper.py
+++ challenge_day1/jesper.py
@@ -13,3 +13,9 @@ At the beach, everyone paused their activities to watch the sun dip below the ho
 Elisa, Jesper, Martyna, Dima, and Sari admired the stunning sunset's colors reflecting on the water. 
 It was a moment of shared wonder and appreciation for nature's beauty, creating memories they would cherish forever.
 '''
+jesper_paragraph3 = '''
+In the moonlit darkness, their unresolved conflicts festered. Elisa couldn't forgive Jesper's betrayal, and Martyna and Dima harbored deep resentments.
+Sari felt helpless as their friendship crumbled.
+As the night grew darker, they silently dispersed, the beach now marred by their broken bonds.
+Under the unforgiving moon, their friendship faded into irreparable disarray, leaving only bitter memories in its wake.
+'''
