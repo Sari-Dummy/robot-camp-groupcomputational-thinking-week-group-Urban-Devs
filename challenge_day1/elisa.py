fbae482bae806504f85210fadc6d8f5ba4d863b3
elisamhh <elisamhurt@gmail.com>
2023-09-04 15:19:06 +0200

    Added second paragraph

--- challenge_day1/elisa.py
+++ challenge_day1/elisa.py
@@ -6,3 +6,11 @@ Elisa the mushroom provided shelter and nourishment, Jesper the conifer offered 
 and Dima the succulent thrived in arid conditions. They stood united, content with their differences,
 and found purpose in their unique roles in the forest.
 '''
+
+elisa_paragraph2 = '''
+As the months passed, Elisa, Jesper, Martyna, Dima, and Sari continued their sunset-watching tradition,
+strengthening the bond of their friendship with each passing day.
+They found solace in the consistent beauty of nature's evening spectacle and shared their dreams, fears, and aspirations beneath the
+colorful skiiiiiiiiiiiiiiiiiiiiiiiies. These sunsets became a symbol of the enduring connection between Sari, Elisa, Jesper, Dima, and Martyna,
+a testament to the lasting power of their shared moments of wooooooooooooooooooooooooooooooooonder.
+'''
