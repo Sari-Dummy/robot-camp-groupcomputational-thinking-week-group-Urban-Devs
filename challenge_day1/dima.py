8452c3f67d327b7e8b8f39a012c3471ace67472f
golovchits <dmitry.golovchits@student.uva.nl>
2023-09-04 16:03:40 +0200

    added story

--- challenge_day1/dima.py
+++ challenge_day1/dima.py
@@ -9,3 +9,10 @@ dima_paragraph2 = '''
 They decided to start a tradition of watching sunsets together whenever they could, 
 a bond forged by nature's breathtaking display. Each subsequent sunset became a cherished moment in Sari's, Elisa's, Jesper's, Dima's and Martyna's friendship.
 '''
+
+dima_paragraph3 = '''
+Weeeeeeeeks had passed since that fateful night on the beach, and the once tight-knit group of friends had drifted even further apart. 
+Elisa had tried to reach out to Jesper, hoping for some sort of reconciliation, but her attempts were met with cold indifference.
+Martyna and Dima, on the other hand, had withdrawnnnnnnnnnnnn from the group entirely.
+Sari, the one who had once held them all together, felt a heavy burden of guilt and sadness. 
+'''
