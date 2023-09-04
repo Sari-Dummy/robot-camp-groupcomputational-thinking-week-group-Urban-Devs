1c3e5f3b223c0868c4e5f8802ed93eb8235c0a73
wmartynaa <wojmartynaa@gmail.com>
2023-09-04 15:35:19 +0200

    added second paragraph

--- challenge_day1/martyna_name.py
+++ challenge_day1/martyna_name.py
@@ -4,3 +4,12 @@ martyna_paragraph1 = '''
 As the seasons changed, Sari, Martyna, Elisa, Jesper, and Dima continued to embrace their unique roles in the forest.
 To celebrate their differences they decided to go to the beach and have fun.
 '''
+
+martyna_paragraph2 = '''
+The friends' tradition of watching sunsets together grew into something truly specialllllll.
+However, one evening the once serene sunset turned into a dramatic spectacleeeee. 
+Elisa and Jesper's friendship unraveled, bitterness poisoning their interactions. 
+Martyna and Dima's clashes over sunset locations escalated into shouting matches, 
+and Sari, torn between loyalties, found herself in the crossfire of their animosity. 
+As the last rays of sunlight were disappearing, the tensions were rising.
+'''
