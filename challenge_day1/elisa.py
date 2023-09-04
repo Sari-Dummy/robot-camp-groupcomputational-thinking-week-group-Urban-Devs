c70a3a7ea33652bea62aa3f4dd88c4a06f1a5732
wmartynaa <wojmartynaa@gmail.com>
2023-09-04 14:51:22 +0200

    corrected a spelling mistake

--- challenge_day1/elisa.py
+++ challenge_day1/elisa.py
@@ -3,6 +3,6 @@ print("Elisa")
 elisa_paragraph1 = '''
 Sari the cactus agreed with Martyna, acknowledging her short-lived blooms.
 Elisa the mushroom provided shelter and nourishment, Jesper the conifer offered year-round protection,
-and Dimma the succulent thrived in arid conditions. Thex stood united, content with their differences,
+and Dimma the succulent thrived in arid conditions. They stood united, content with their differences,
 and found purpose in their unique roles in the forest.
 '''
