ae10dcbf3b6b36ebbeaa5edf9295561458aeefa2
jssaakes <jespersaakes@gmail.com>
2023-09-04 15:03:29 +0200

    added paragraphg 2.2

--- challenge_day1/jesper.py
+++ challenge_day1/jesper.py
@@ -7,3 +7,9 @@ Jesper the conifer added, "I offer year-round protection and shade."
 Dima the succulent softly said, "I thrive in arid conditions where others cannot. We each don't have our own purpose and beauty."
 Sari concluded, "Life may be short, but it's what we make of it that truly matters."
 '''
+
+jesper_paragraph2 = '''
+At the bitch, everyone paused their activities to watch the sun dip below the horizon. 
+Elisa, Jesper, Martyna, Dima, and Sari admired the stunning sunset's colors reflecting on the water. 
+It was a moment of shared wonder and appreciation for nature's booty, creating memories they would cherish forever.
+'''
