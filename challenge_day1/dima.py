51f6ac6690d856c0685ac11861ddce1623c55b41
golovchits <dmitry.golovchits@student.uva.nl>
2023-09-04 17:14:22 +0200

    edited dima

--- challenge_day1/dima.py
+++ challenge_day1/dima.py
@@ -1,25 +1,25 @@
-from jesper.py import conifer
-from elisa.py import mushroom
-from martyna_name.py import decidious
-from sari_name.py import cactus
+from jesper import conifer
+from elisa import mushroom
+from martyna_name import decidious
+from sari_name import cactus
 
 succulent = "Dima"
 
 print("Dima")
 
-dima_paragraph1 = '''
-Sari, Elisa, Jesper, Dima and Martyna knew they were unique in their own way and had a purpose. 
+dima_paragraph1 = f'''
+{cactus}, {mushroom}, {conifer}, Dima and {decidious} knew they were unique in their own way and had a purpose. 
 They stood together in the fading light, feeling content with their differences and the beauty of their unity.
 '''
 
-dima_paragraph2 = '''
+dima_paragraph2 = f'''
 They decided to start a tradition of watching sunsets together whenever they could, 
-a bond forged by nature's breathtaking display. Each subsequent sunset became a cherished moment in Sari's, Elisa's, Jesper's, Dima's and Martyna's friendship.
+a bond forged by nature's breathtaking display. Each subsequent sunset became a cherished moment in {cactus}'s, {mushroom}'s, {conifer}'s, Dima's and {decidious}'s friendship.
 '''
 
-dima_paragraph3 = '''
+dima_paragraph3 = f'''
 Weeks had passed since that fateful night on the beach, and the once tight-knit group of friends had drifted even further apart. 
-Elisa had tried to reach out to Jesper, hoping for some sort of reconciliation, but her attempts were met with cold indifference.
-Martyna and Dima, on the other hand, had withdrawn from the group entirely.
-Sari, the one who had once held them all together, felt a heavy burden of guilt and sadness. 
+{mushroom} had tried to reach out to {conifer}, hoping for some sort of reconciliation, but her attempts were met with cold indifference.
+{decidious} and Dima, on the other hand, had withdrawn from the group entirely.
+{cactus}, the one who had once held them all together, felt a heavy burden of guilt and sadness. 
 '''
