a602de4040f2673369d79002d6403fb9ec35f37e
elisamhh <elisamhurt@gmail.com>
2023-09-04 15:41:53 +0200

    Added Sari's final paragraph

--- challenge_day1/sari_name.py
+++ challenge_day1/sari_name.py
@@ -12,3 +12,11 @@ At the beach everyone was extremely happy. Elisa jumped over the waves. Jesper w
 Martyna was fishing. Dima and Sari were making sandcastles. The sunset was approaching and
 the sunlight reflected in the water.
 '''
+
+sari_paragraph3 = '''
+The sunset turned sinister as Jesper's shocking confession about secretly dating Elisa's sister tore their friendship apart.
+Elisa's heartache was palpable, while nearby, Martyna and Dima's feud boiled over
+when Martyna defiantly unfolded her carefully drawn map, leading to a fIrEy confrontation.
+Sari, caught in the crossfire, felttttttttttt their cherished tradition disintegrate,
+a bittersweet memory of happier days as the sun dipped below the horizon.
+'''
