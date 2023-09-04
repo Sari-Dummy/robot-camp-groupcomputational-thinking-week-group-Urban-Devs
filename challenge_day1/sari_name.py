ea4ac3e324d490846c337b3a9315d779d2081a81
wmartynaa <wojmartynaa@gmail.com>
2023-09-04 17:17:49 +0200

    replaced names

--- challenge_day1/sari_name.py
+++ challenge_day1/sari_name.py
@@ -1,22 +1,27 @@
+from dima import succulent
+from elisa import mushroom
+from jesper import conifer
+from martyna_name import decidious
+
 print("Sari")
 cactus = "Sari"
-sari_paragraph1 = '''
-There was once a called Sari, a mushroom called Elisa, a conifer called Jesper,
-a deciduous called Martyna, and a succulent called Dima.
+sari_paragraph1 = f'''
+There was once a called Sari, a mushroom called {mushroom}, a conifer called {conifer},
+a deciduous called {decidious}, and a succulent called {succulent}.
 "Your flower is so short-lived, Sari" the deciduous said, "as a Cactus it only blooms once
 and then wilts off."
 '''
 
-sari_paragraph2 = '''
-At the beach everyone was extremely happy. Elisa jumped over the waves. Jesper was diving. 
-Martyna was fishing. Dima and Sari were making sandcastles. The sunset was approaching and
+sari_paragraph2 = f'''
+At the beach everyone was extremely happy. {mushroom} jumped over the waves. {conifer} was diving. 
+{decidious} was fishing. {succulent} and Sari were making sandcastles. The sunset was approaching and
 the sunlight reflected in the water.
 '''
 
-sari_paragraph3 = '''
-The sunset turned sinister as Jesper's shocking confession about secretly dating Elisa's sister tore their friendship apart.
-Elisa's heartache was palpable, while nearby, Martyna and Dima's feud boiled over
-when Martyna defiantly unfolded her carefully drawn map, leading to a firey confrontation.
+sari_paragraph3 = f'''
+The sunset turned sinister as {conifer}'s shocking confession about secretly dating Elisa's sister tore their friendship apart.
+Elisa's heartache was palpable, while nearby, {decidious} and {succulent}'s feud boiled over
+when {decidious} defiantly unfolded her carefully drawn map, leading to a firey confrontation.
 Sari, caught in the crossfire, felt their cherished tradition disintegrate,
 a bittersweet memory of happier days as the sun dipped below the horizon.
 '''
