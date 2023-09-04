54eb2da0d1114a312e4f5fb528b9df2cdc8d8655
wmartynaa <wojmartynaa@gmail.com>
2023-09-04 17:04:33 +0200

    added imports

--- challenge_day1/martyna_name.py
+++ challenge_day1/martyna_name.py
@@ -1,6 +1,11 @@
+from dima.py import succulent
+from elisa.py import mushroom
+from jesper.py import conifer
+from sari_name.py import cactus
+
 print("Martyna")
 
-deciduous = "Martyna"
+decidious = "Martyna"
 
 martyna_paragraph1 = '''
 As the seasons changed, Sari, Martyna, Elisa, Jesper, and Dima continued to embrace their unique roles in the forest.
