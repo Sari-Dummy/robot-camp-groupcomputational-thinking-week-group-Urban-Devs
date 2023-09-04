a88c6150a1b6f05652534317155ab017dd25c5d4
elisamhh <elisamhurt@gmail.com>
2023-09-04 13:59:29 +0200

    added elisa.py

--- challenge_day1/git_exercise.py
+++ challenge_day1/git_exercise.py
@@ -2,6 +2,8 @@ import dima.py
 import sari_name.py
 import jesper.py
 import martyna_name.py
+import elisa.py
+
 print('This is Team YourTeamNameHere. We are:')
 def git_exercise():
     print('This is Team Urban devs. We are:')
