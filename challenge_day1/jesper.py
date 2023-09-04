598eb4780378a3ebf09d078fd28e116b422d5b13
elisamhh <elisamhurt@gmail.com>
2023-09-04 14:52:48 +0200

    Corrected Jesper's error

--- challenge_day1/jesper.py
+++ challenge_day1/jesper.py
@@ -4,6 +4,6 @@ jesper_paragraph1 = ''''
 Sari the cactus agreed with Martyna, "Yes, I only bloom once, but I cherish those moments."
 Elisa the mushroom mentioned, "I provide shelter and nourishment to forest creatures."
 Jesper the conifer added, "I offer year-round protection and shade."
-Dima the succulent softly said, "I thrive in arid conditions where others cannot. We each have our own purpose and beauty."
+Dima the succulent softly said, "I thrive in arid conditions where others cannot. We each don't have our own purpose and beauty."
 Sari concluded, "Life may be short, but it's what we make of it that truly matters."
 '''
