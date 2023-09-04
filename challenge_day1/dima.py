9531ee7d9ff0f70e224698ebb9caa270d2550128
elisamhh <elisamhurt@gmail.com>
2023-09-04 16:04:31 +0200

    Fixed DIma's mistake

--- challenge_day1/dima.py
+++ challenge_day1/dima.py
@@ -11,7 +11,7 @@ a bond forged by nature's breathtaking display. Each subsequent sunset became a 
 '''
 
 dima_paragraph3 = '''
-Weeeeeeeeks had passed since that fateful night on the beach, and the once tight-knit group of friends had drifted even further apart. 
+Weeks had passed since that fateful night on the beach, and the once tight-knit group of friends had drifted even further apart. 
 Elisa had tried to reach out to Jesper, hoping for some sort of reconciliation, but her attempts were met with cold indifference.
 Martyna and Dima, on the other hand, had withdrawnnnnnnnnnnnn from the group entirely.
 Sari, the one who had once held them all together, felt a heavy burden of guilt and sadness. 
