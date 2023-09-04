af76632b96003340359b7d793d568bbd0e5da972
jssaakes <jespersaakes@gmail.com>
2023-09-04 14:27:45 +0200

    added paragraphg 1.2

--- challenge_day1/jesper.py
+++ challenge_day1/jesper.py
@@ -1,1 +1,9 @@
 print('Jesper')
+
+jesper_paragraph1 = ''''
+Sari the cactus agreed with Martyna, "Yes, I only bloom once, but I cherish those moments."
+Elisa the mushroom mentioned, "I provide shelter and nourishment to forest creatures."
+Jesper the conifer added, "I offer year-round protection and shade."
+Dima the succulent softly said, "I thrive in arid conditions where others cannot. We each have our own purpose and beauty."
+Sari concluderd, "Life may be short, but it's what we make of it that truly matters."
+'''
