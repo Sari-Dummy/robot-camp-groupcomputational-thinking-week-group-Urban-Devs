ad78331f174c02e986c8e6fe744ce00fa7d4d79b
elisamhh <elisamhurt@gmail.com>
2023-09-04 16:35:22 +0200

    Fixed an error

--- challenge_day1/git_exercise.py
+++ challenge_day1/git_exercise.py
@@ -31,4 +31,4 @@ act3_paragraph3 = dima.dima_paragraph3
 act3_paragraph4 = elisa.elisa_paragraph3
 act3_paragraph5 = martyna_name.martyna_paragraph3
 
-print(act1_paragraph1 + "\n" + act1_paragraph2 + "\n" + act1_paragraph3 + "\n" + act1_paragraph4 + "\n" + act1_paragraph5 + "\n" + "Act 2" + "\n" + act2_paragraph1 + "\n" + act2_paragraph2 + "\n" + act2_paragraph3 + "\n" + act2_paragraph4 + "\n" + act2_paragraph5 + "\n" + "Act 3" + "\n" + act3_paragraph1 + "\n" + act3_paragraph2 + "\n" + act3_paragraph3 + "\n" + act3_paragraph4 + "\n" + act3_paragraph5)
+print("Act 1" + "\n" + act1_paragraph1 + "\n" + act1_paragraph2 + "\n" + act1_paragraph3 + "\n" + act1_paragraph4 + "\n" + act1_paragraph5 + "\n" + "Act 2" + "\n" + act2_paragraph1 + "\n" + act2_paragraph2 + "\n" + act2_paragraph3 + "\n" + act2_paragraph4 + "\n" + act2_paragraph5 + "\n" + "Act 3" + "\n" + act3_paragraph1 + "\n" + act3_paragraph2 + "\n" + act3_paragraph3 + "\n" + act3_paragraph4 + "\n" + act3_paragraph5)
