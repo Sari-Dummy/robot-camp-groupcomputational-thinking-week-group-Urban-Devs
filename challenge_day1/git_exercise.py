0826c150449e7223f3645ccc7365d25c43af9e29
elisamhh <elisamhurt@gmail.com>
2023-09-04 16:33:45 +0200

    Added the code to print paragraphs

--- challenge_day1/git_exercise.py
+++ challenge_day1/git_exercise.py
@@ -1,4 +1,9 @@
 import subprocess
+import sari_name
+import jesper
+import dima
+import elisa
+import martyna_name
 
 print('This is Team Urban devs. We are:')
 
@@ -7,3 +12,23 @@ subprocess.run(['python', 'sari_name.py'])
 subprocess.run(['python', 'jesper.py'])
 subprocess.run(['python', 'elisa.py'])
 subprocess.run(['python', 'martyna_name.py'])
+
+
+
+act1_paragraph1 = sari_name.sari_paragraph1 
+act1_paragraph2 = jesper.jesper_paragraph1
+act1_paragraph3 = dima.dima_paragraph1
+act1_paragraph4 = elisa.elisa_paragraph1
+act1_paragraph5 = martyna_name.martyna_paragraph1
+act2_paragraph1 = sari_name.sari_paragraph2
+act2_paragraph2 = jesper.jesper_paragraph2
+act2_paragraph3 = dima.dima_paragraph2
+act2_paragraph4 = elisa.elisa_paragraph2
+act2_paragraph5 = martyna_name.martyna_paragraph2
+act3_paragraph1 = sari_name.sari_paragraph3
+act3_paragraph2 = jesper.jesper_paragraph3
+act3_paragraph3 = dima.dima_paragraph3
+act3_paragraph4 = elisa.elisa_paragraph3
+act3_paragraph5 = martyna_name.martyna_paragraph3
+
+print(act1_paragraph1 + "\n" + act1_paragraph2 + "\n" + act1_paragraph3 + "\n" + act1_paragraph4 + "\n" + act1_paragraph5 + "\n" + "Act 2" + "\n" + act2_paragraph1 + "\n" + act2_paragraph2 + "\n" + act2_paragraph3 + "\n" + act2_paragraph4 + "\n" + act2_paragraph5 + "\n" + "Act 3" + "\n" + act3_paragraph1 + "\n" + act3_paragraph2 + "\n" + act3_paragraph3 + "\n" + act3_paragraph4 + "\n" + act3_paragraph5)
