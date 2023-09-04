f2749fc07ee1f926170ae2fa7a13605df4e80888
wmartynaa <wojmartynaa@gmail.com>
2023-09-04 17:12:32 +0200

    replaced names

--- challenge_day1/martyna_name.py
+++ challenge_day1/martyna_name.py
@@ -12,18 +12,18 @@ As the seasons changed, {cactus}, Martyna, {mushroom}, {conifer}, and {succulent
 To celebrate their differences they decided to go to the beach and have fun.
 '''
 
-martyna_paragraph2 = '''
+martyna_paragraph2 = f'''
 The friends' tradition of watching sunsets together grew into something truly special.
 However, one evening the once serene sunset turned into a dramatic spectacle. 
-Elisa and Jesper's friendship unraveled, bitterness poisoning their interactions. 
-Martyna and Dima's clashes over sunset locations escalated into shouting matches, 
-and Sari, torn between loyalties, found herself in the crossfire of their animosity. 
+{mushroom} and {conifer}'s friendship unraveled, bitterness poisoning their interactions. 
+Martyna and {succulent}'s clashes over sunset locations escalated into shouting matches, 
+and {cactus}, torn between loyalties, found herself in the crossfire of their animosity. 
 As the last rays of sunlight were disappearing, the tensions were rising.
 '''
 
-martyna_paragraph3 = '''
-Unfortunately, Elisa and Jesper never reconciled even though Sari tried her best to bring their friendship back. 
-The withdrawal of Dima led to him finally leaving the country and Martyna decided to disappear as well, she sailed out into the darkness.
+martyna_paragraph3 = f'''
+Unfortunately, {mushroom} and {conifer} never reconciled even though {cactus} tried her best to bring their friendship back. 
+The withdrawal of {succulent} led to him finally leaving the country and Martyna decided to disappear as well, she sailed out into the darkness.
 
 By the way, they were still all plants (except for that one mushroom). That just got lost along the way. What with the beach stuff happening.
 But they were definitely plants. With the chloroplast and everything. Did you know that half of cacti are pollinated
