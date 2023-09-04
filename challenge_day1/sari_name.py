6d1d0992c470a29c512693ba99360e6e55251eb8
wmartynaa <wojmartynaa@gmail.com>
2023-09-04 14:46:48 +0200

    corrected spelling mistake

--- challenge_day1/sari_name.py
+++ challenge_day1/sari_name.py
@@ -1,7 +1,7 @@
 print("Sari")
 
 sari_paragraph1 = '''
-There was once a cactus calld Sari, a mushroom called Elisa, a conifer called Jesper,
+There was once a cactus called Sari, a mushroom called Elisa, a conifer called Jesper,
 a deciduous called Martyna, and a succulent called Dima.
 "Your flower is so short-lived, Sari" the deciduous said, "as a Cactus it only blooms once
 and then wilts off."
