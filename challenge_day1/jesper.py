5cc2a9a564dab142e73668e35cbb8ad8b351441f
golovchits <dmitry.golovchits@student.uva.nl>
2023-09-04 18:07:55 +0200

    edited

--- challenge_day1/jesper.py
+++ challenge_day1/jesper.py
@@ -2,7 +2,7 @@ conifer = 'Jesper'
 
 print('Jesper')
 
-def jesper_paragraph1(cactus, decidious, mushroom, conifer):
+def jesper_paragraph1(cactus, decidious, mushroom, succulent):
     jesper_paragraph1 = f'''
     {cactus} the cactus agreed with {decidious}, "Yes, I only bloom once, but I cherish those moments."
     {mushroom} the mushroom mentioned, "I provide shelter and nourishment to forest creatures."
@@ -11,15 +11,15 @@ def jesper_paragraph1(cactus, decidious, mushroom, conifer):
     {cactus} concluded, "Life may be short, but it's what we make of it that truly matters."
     '''
 
-def jesper_paragraph2(cactus, decidious, mushroom, conifer):
+def jesper_paragraph2(cactus, decidious, mushroom, succulent):
     jesper_paragraph2 = f'''
     At the beach, everyone paused their activities to watch the sun dip below the horizon. 
     {mushroom}, {conifer}, {decidious}, {succulent}, and {cactus} admired the stunning sunset's colors reflecting on the water. 
     It was a moment of shared wonder and appreciation for nature's beauty, creating memories they would cherish forever.
     '''
-def jesper_paragraph3(cactus, decidious, mushroom, conifer):
+def jesper_paragraph3(cactus, decidious, mushroom, succulent):
     jesper_paragraph3 = f'''
-    In the moonlit darkness, their unresolved conflicts festered. {mushroom} couldn't forgive {conifer}'s betrayal, and {decidious} and {succulent} harbored deep resentments.
+    In the moonlit darkness, their unresolved conflicts festered. {mushroom} couldn't forgive {succulent}'s betrayal, and {decidious} and {succulent} harbored deep resentments.
     {cactus} felt helpless as their friendship crumbled.
     As the night grew darker, they silently dispersed, the beach now marred by their broken bonds.
     Under the unforgiving moon, their friendship faded into irreparable disarray, leaving only bitter memories in its wake.
