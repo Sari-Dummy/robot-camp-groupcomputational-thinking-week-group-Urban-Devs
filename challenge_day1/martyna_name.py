4262e7bc664a26135eddecf3a977bdd55ed2a5f6
wmartynaa <wojmartynaa@gmail.com>
2023-09-04 16:59:14 +0200

    added a variable

--- challenge_day1/martyna_name.py
+++ challenge_day1/martyna_name.py
@@ -1,5 +1,7 @@
 print("Martyna")
 
+deciduous = "Martyna"
+
 martyna_paragraph1 = '''
 As the seasons changed, Sari, Martyna, Elisa, Jesper, and Dima continued to embrace their unique roles in the forest.
 To celebrate their differences they decided to go to the beach and have fun.
