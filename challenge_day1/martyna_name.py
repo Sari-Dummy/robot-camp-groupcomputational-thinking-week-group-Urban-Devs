f489e1b55cc7c1ee82ecc55bd3fe1ff2c3fadb20
wmartynaa <wojmartynaa@gmail.com>
2023-09-04 14:54:18 +0200

    added my first paragraph

--- challenge_day1/martyna_name.py
+++ challenge_day1/martyna_name.py
@@ -1,1 +1,6 @@
 print("Martyna")
+
+martyna_paragraph1 = '''
+As the seasons changed, Sari, Martyna, Elisa, Jesper, and Dima continued to embrace their unique roles in the forest.
+To celebrate their difffferences they decided to go to the beach and have funnn.
+'''
