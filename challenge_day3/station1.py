8cf90822b9fd4e08a19d59168a6e5e8555944579
elisamhh <elisamhurt@gmail.com>
2023-09-06 13:06:17 +0200

    added solution_station_1

---
+++ challenge_day3/station1.py
@@ -0,0 +1,14 @@
+def solution_station_1(position):
+    if position <= 0:
+        return 0
+    elif position == 1:
+        return 1
+    else:
+        fib = [0] * (position + 1)
+        fib[1] = 1
+        for i in range(2, position + 1):
+            fib[i] = fib[i - 1] + fib[i - 2]
+        return fib[position]
+
+
+
